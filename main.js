152ca4f60e3d1f60fd140b1f9ca5d71185fe8ec3
Chris Kamp <chriskamp101@gmail.com>
2020-09-20 16:06:08 +1000

    Change 'book' to 'card' in class names for consistency

--- main.js
+++ main.js
@@ -11,25 +11,25 @@ function Book(title, author, pages, read) {
 function addBook(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     const newCard = document.createElement("div");
-    newCard.classList.add("book");
-    const bookHeader = document.createElement("div");
-    bookHeader.classList.add("bookHeader");
+    newCard.classList.add("card");
+    const cardHeader = document.createElement("div");
+    cardHeader.classList.add("cardHeader");
     const titlePara = document.createElement("p");
     titlePara.textContent = newBook.title;
     const authorPara = document.createElement("p");
     authorPara.textContent = newBook.author;
-    const bookInfo = document.createElement("div");
-    bookInfo.classList.add("bookInfo");
+    const cardInfo = document.createElement("div");
+    cardInfo.classList.add("cardInfo");
     const pagesPara = document.createElement("p");
     pagesPara.textContent = newBook.pages;
     const readPara = document.createElement("p");
     readPara.textContent = newBook.read;
     gridContainer.appendChild(newCard);
-    newCard.appendChild(bookHeader);
-    bookHeader.appendChild(titlePara);
-    bookHeader.appendChild(authorPara);
-    newCard.appendChild(bookInfo);
-    bookInfo.appendChild(pagesPara);
-    bookInfo.appendChild(readPara);
+    newCard.appendChild(cardHeader);
+    cardHeader.appendChild(titlePara);
+    cardHeader.appendChild(authorPara);
+    newCard.appendChild(cardInfo);
+    cardInfo.appendChild(pagesPara);
+    cardInfo.appendChild(readPara);
     bookshelf.push(newBook);
 }
