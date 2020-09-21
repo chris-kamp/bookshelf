0f4fd766f26141791561c4e691c984a3b795357c
Chris Kamp <chriskamp101@gmail.com>
2020-09-21 20:51:48 +1000

    Add button to toggle read status

--- main.js
+++ main.js
@@ -26,23 +26,43 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+//Toggle whether a book has been read
+Book.prototype.toggleRead = function () {
+    this.read = !this.read;
+    updateDisplay();
+}
+
 //Add a new book to the bookshelf
 function addBook(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     const newCard = document.createElement("div");
     newCard.classList.add("card");
+    newCard.setAttribute("data-index", bookshelf.length);
     const cardHeader = document.createElement("div");
     cardHeader.classList.add("cardHeader");
     const titlePara = document.createElement("p");
+    titlePara.classList.add("titlePara");
     titlePara.textContent = newBook.title;
     const authorPara = document.createElement("p");
+    authorPara.classList.add("authorPara");
     authorPara.textContent = newBook.author;
     const cardInfo = document.createElement("div");
     cardInfo.classList.add("cardInfo");
     const pagesPara = document.createElement("p");
+    pagesPara.classList.add("pagesPara");
     pagesPara.textContent = newBook.pages;
     const readPara = document.createElement("p");
-    readPara.textContent = newBook.read;
+    readPara.classList.add("readPara");
+    readPara.textContent = "Read? " + newBook.read;
+    const readButton = document.createElement("button");
+
+    //When the toggle read button is clicked, toggle whether the relevant book has been read
+    readButton.addEventListener("click", (e) => {
+        const index = e.target.parentElement.parentElement.getAttribute("data-index");
+        bookshelf[index].toggleRead();
+    });
+
+    readButton.textContent = "Toggle read";
     gridContainer.appendChild(newCard);
     newCard.appendChild(cardHeader);
     cardHeader.appendChild(titlePara);
@@ -50,6 +70,7 @@ function addBook(title, author, pages, read) {
     newCard.appendChild(cardInfo);
     cardInfo.appendChild(pagesPara);
     cardInfo.appendChild(readPara);
+    cardInfo.appendChild(readButton);
     bookshelf.push(newBook);
 }
 
@@ -58,7 +79,7 @@ function getBook() {
     const title = titleInput.value === "" ? "UNTITLED" : titleInput.value.toUpperCase();
     const author = authorInput.value === "" ? "Author unknown" : "By " + authorInput.value;
     const pages = pagesInput.value === "" ? "Pages: Unknown" : "Pages: " + pagesInput.value;
-    const read = readInput.checked ? "Read" : "Unread";
+    const read = readInput.checked;
     addBook(title, author, pages, read);
 }
 
@@ -68,5 +89,26 @@ function clearForm() {
     authorInput.value = "";
     pagesInput.value = "";
     readInput.checked = false;
+}
+
+//Update the display of each card
+function updateDisplay() {
+    const cards = Array.from(document.getElementsByClassName("card"));
+    cards.forEach(card => {
+        index = card.getAttribute("data-index");
+        if(!index) {
+            return;
+        }
+        const titlePara = card.getElementsByClassName("titlePara");
+        const authorPara = card.getElementsByClassName("authorPara");
+        const pagesPara = card.getElementsByClassName("pagesPara");
+        const readPara = card.getElementsByClassName("readPara");
+        
+        titlePara[0].textContent = bookshelf[index].title;
+        authorPara[0].textContent = bookshelf[index].author;
+        pagesPara[0].textContent = bookshelf[index].pages;
+        readPara[0].textContent = bookshelf[index].read;
+    })
+
 }
 
